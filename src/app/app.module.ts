1243ab97bec082d2a541e1897c4df5cbd621226f
Jan <hiasguy@hotmail.com>
2021-10-17 16:22:23 +0200

    added skills section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
