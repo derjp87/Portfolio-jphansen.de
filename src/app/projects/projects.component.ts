6aedbee5177bddcd6f1a1ffa91dd0370957f3281
Jan <hiasguy@hotmail.com>
2021-11-13 16:25:52 +0100

    added select function on projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,15 +10,41 @@ export class ProjectsComponent implements OnInit {
   showDetails2: boolean;
   showDetails3: boolean;
   showDetails4: boolean;
+
+  showAll: boolean;
+  showAngular: boolean;
+  showJavaScript: boolean;
+
   constructor() { 
     this.showDetails1 = false;
     this.showDetails2 = false;
     this.showDetails3 = false;
     this.showDetails4 = false;
+    this.showAll = true;
+    this.showAngular = false;
+    this.showJavaScript = false;
     
   }
 
   ngOnInit(): void {
+  }
+
+  showAllProjects() {
+    this.showAll = true;
+    this.showAngular = true;
+    this.showJavaScript = true;
+  }
+
+  showAngularProjects() {
+    this.showAll = false;
+    this.showAngular = true;
+    this.showJavaScript = false;
+  }
+
+  showJavaScriptProjects() {
+    this.showAll = false;
+    this.showAngular = false;
+    this.showJavaScript = true;
   }
   
 
