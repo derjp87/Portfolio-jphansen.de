63ad6f0ce2930ff064ea6a8172c286e128172a1b
Jan <hiasguy@hotmail.com>
2021-11-28 18:08:51 +0100

    fixed contact send mail function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ImprintComponent } from './imprint/imprint.component';
 import { DataprotectionComponent } from './dataprotection/dataprotection.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -33,15 +33,39 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 export class ContactComponent implements OnInit {
 
   mailsuccess: boolean;
+  name = '';
+  email = '';
+  message = '';
+  sendMailUrl;
+  
 
   constructor(public ls: LanguageService) {
     this.mailsuccess = false;
+    this.name = this.name;
+    this.email = this.email;
+    this.message = this.message;
+    this.sendMailUrl = 'https://www.jphansen.de/send_mail.php';
    }
 
   ngOnInit(): void {
   }
 
+  //senden() {
+    //this.mailsuccess = true;
+    //return false;
+  //}
+
   senden() {
+    let formData = new FormData()
+      formData.append('name', this.name);
+      formData.append('email', this.email);
+      formData.append('message', this.message);
+
+    fetch(this.sendMailUrl, {
+      method: "POST",
+      body: formData
+    })
+
     this.mailsuccess = true;
     return false;
   }
@@ -51,4 +75,6 @@ export class ContactComponent implements OnInit {
   }
 
 }
+
+
 
